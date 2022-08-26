3e1af649a2da47c559869da6b1ff6ecced8f0da4
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-26 09:35:43 -0700

    Initial Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,9 @@
+﻿var videoGames = new List<string>();
+
+videoGames.Add("Total War Warhammer");
+videoGames.Add("Halo");
+videoGames.Add("The Witcher");
+videoGames.Add("Mass Effect");
+videoGames.Add("Assassin's Creed");
+
+videoGames.OrderBy(x => x.Length).ToList().ForEach(Console.WriteLine);
